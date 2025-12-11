5d73cf69602f561916a98378526c0048f02fd073
Jeremy <ggajy123@gmail.com>
2025-12-11 10:27:38 -0800

    Camera transition between room

---
+++ Assets/Scripts/RoomTransition/RoomCameraLimit.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class RoomCameraLimit : MonoBehaviour
+{
+    // Public getter for the collider bounds
+    public Collider2D RoomCollider { get; private set; }
+
+    private void Awake()
+    {
+        RoomCollider = GetComponent<Collider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // When the player enters this trigger, tell the camera to switch to this room
+        if (collision.CompareTag("Player"))
+        {
+            RoomCameraController.Instance.TransitionToRoom(this);
+        }
+    }
+}
